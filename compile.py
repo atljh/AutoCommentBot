64d2de10e487895d5113c36343eff042b37efce5
Федор Лукашов <fiodorlukashov@gmail.com>
2024-11-29 07:42:51 -0800

    Fix compile script

--- compile.py
+++ compile.py
@@ -4,13 +4,12 @@ import shutil
 import subprocess
 from pathlib import Path
 
-main_script = Path(__file__).parent.parent / "main.py" 
-if not main_script.exists():
-    main_script = Path(__file__).parent.parent / "app.py" 
+main_script = "main.py"
+if not Path(main_script).exists():
+    main_script = "app.py"
 
 APP_NAME = Path.cwd().name + ".exe"
 
-dist_dir = Path(__file__).parent.parent / "dist"
 
 def compile_by_pyinstaller():
     """Компиляция"""
@@ -22,25 +21,22 @@ def compile_by_pyinstaller():
         # noinspection PyPackageRequirements
         import PyInstaller.__main__  # type:ignore
 
-    cmd = [
-        str(main_script),
-        "--distpath", str(dist_dir) 
-    ]
+    cmd = [main_script, "-F"]
     PyInstaller.__main__.run(cmd)
 
 
 def after_compile_clean_and_rename():
     """Очистка и переименовывание"""
-    distfile = dist_dir / f"{main_script.stem}.exe"  
+    distfile = Path(f"dist/{main_script.replace('.py', '.exe')}")
     app_path = Path(APP_NAME)
     if distfile.exists():
         if app_path.exists():
             os.remove(APP_NAME)
         os.rename(distfile, APP_NAME)
         with contextlib.suppress(Exception):
-            os.removedirs(str(dist_dir))
+            os.removedirs("dist")
     with contextlib.suppress(Exception):
-        os.remove(main_script.with_suffix(".spec").name)
+        os.remove(main_script.replace(".py", ".spec"))
     with contextlib.suppress(Exception):
         shutil.rmtree("build", ignore_errors=True)
 
