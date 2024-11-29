b8cf58186fd01688e3d2f1027202636c39cd6c38
Федор Лукашов <fiodorlukashov@gmail.com>
2024-11-29 14:44:42 -0800

    Edit compile script to find main.py

--- scripts/compile.py
+++ scripts/compile.py
@@ -4,12 +4,13 @@ import shutil
 import subprocess
 from pathlib import Path
 
-main_script = "main.py"
-if not Path(main_script).exists():
-    main_script = "app.py"
+main_script = Path(__file__).parent.parent / "main.py" 
+if not main_script.exists():
+    main_script = Path(__file__).parent.parent / "app.py" 
 
 APP_NAME = Path.cwd().name + ".exe"
 
+dist_dir = Path(__file__).parent.parent / "dist"
 
 def compile_by_pyinstaller():
     """Компиляция"""
@@ -21,26 +22,25 @@ def compile_by_pyinstaller():
         # noinspection PyPackageRequirements
         import PyInstaller.__main__  # type:ignore
 
-    cmd = [main_script, "-F", "--collect-all=montydb"]
-    for icon in Path(".").glob("*.ico"):
-        cmd.append("-i")
-        cmd.append(icon.name)
-        break
+    cmd = [
+        str(main_script),
+        "--distpath", str(dist_dir) 
+    ]
     PyInstaller.__main__.run(cmd)
 
 
 def after_compile_clean_and_rename():
     """Очистка и переименовывание"""
-    distfile = Path(f"dist/{main_script.replace('.py', '.exe')}")
+    distfile = dist_dir / f"{main_script.stem}.exe"  
     app_path = Path(APP_NAME)
     if distfile.exists():
         if app_path.exists():
             os.remove(APP_NAME)
         os.rename(distfile, APP_NAME)
         with contextlib.suppress(Exception):
-            os.removedirs("dist")
+            os.removedirs(str(dist_dir))
     with contextlib.suppress(Exception):
-        os.remove(main_script.replace(".py", ".spec"))
+        os.remove(main_script.with_suffix(".spec").name)
     with contextlib.suppress(Exception):
         shutil.rmtree("build", ignore_errors=True)
 
