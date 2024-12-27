acd4bbe8c55f264089075a9035ed9e610fb42e25
Fyodor <fiodorlukashov@gmail.com>
2024-12-27 22:58:22 +0200

    Import register func

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import asyncio
 from config import ConfigManager
 from src.starter import Starter
 from src.thon.json_converter import JsonConverter
+from scripts.authorization import register_user
 
 # register_user()
 
